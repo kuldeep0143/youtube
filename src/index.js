fc8f866d5e2fa1a36893ceece444f29aba38c76c
Kuldeep <kuldeepchouhan923@gmail.com>
2024-02-22 14:30:39 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB().then(() => {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log("Sercer is running on port " + process.env.PORT)
+  });
+}).catch((err) => {
+console.error("MONGO DB Connection failed !!",err);
+})
